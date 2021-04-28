4264fd182b5be8a39f35741d008eeba68dcc02ab
panzha <panzha@dian.so>
2021-04-28 19:47:10 +0800

    初始化

---
+++ JDK/JDK1.8/test/main/HelloWold.java
@@ -0,0 +1,15 @@
+package main;
+
+
+import org.junit.Test;
+
+public class HelloWold {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+
+    @Test
+    public void sayHelloWorld() {
+        System.out.println("helloworld");
+    }
+}
